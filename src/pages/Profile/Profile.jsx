a4f4ca2817f8775bb21b5a5fa4f989f03e2fa522
ruwpy <ruwpy.who@gmail.com>
2022-12-21 23:55:51 +0700

    create profile page

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import './Profile.scss'
+
+export default function Profile() {
+  return (
+    <main className="main">
+      <div className="profile container">
+        профиль
+      </div>
+    </main>
+  )
+}
