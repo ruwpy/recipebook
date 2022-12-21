6b95c6e629beabd102131c0d1f1167f9311508d1
ruwpy <ruwpy.who@gmail.com>
2022-12-21 23:56:05 +0700

    create loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import './Loading.scss'
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading__content">
+        <span className='loading__spinner'></span>
+      </div>
+    </div>
+  )
+}
