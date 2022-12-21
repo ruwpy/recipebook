fcd1d4510d7dec7dbbf642c0f4cb0e9aea651023
ruwpy <ruwpy.who@gmail.com>
2022-12-21 20:58:47 +0700

    create global store

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import Navbar from "./components/Navbar/Navbar";
 import Recipes from "./components/Recipes/Recipes";
 import './App.scss'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider as StoreProvider } from 'react-redux';
+import store from "./store/store";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <header className="header">
-        <Search />
-        <Navbar />
-      </header>
-      <Routes>
-        <Route path='/' element={<Recipes />} />
-        <Route path='/profile' element={<Recipes />} />
-        <Route path='/createrecipe' element={<Recipes />} />
-      </Routes>
-    </BrowserRouter>
+    <StoreProvider store={store}>
+      <BrowserRouter>
+        <header className="header">
+          <Search />
+          <Navbar />
+        </header>
+        <Routes>
+          <Route path='/' element={<Recipes />} />
+          <Route path='/profile' element={<Recipes />} />
+          <Route path='/createrecipe' element={<Recipes />} />
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
   );
 }
