5880bce48b3cc04b8335dde18ee40e4d19addb65
ruwpy <ruwpy.who@gmail.com>
2022-12-21 18:16:15 +0700

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import Search from "./components/Search/Search";
 import Navbar from "./components/Navbar/Navbar";
 import Recipes from "./components/Recipes/Recipes";
 import './App.scss'
+import { Router, Routes, Route } from '@solidjs/router';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <header class="header">
+        <Search />
         <Navbar />
       </header>
-      <main class="main">
-        <Recipes />
-      </main>
-    </>
+      <Routes>
+        <Route path='/' element={<Recipes />} />
+        <Route path='/profile' element={<Recipes />} />
+        <Route path='/createrecipe' element={<Recipes />} />
+      </Routes>
+    </Router>
   );
 }
