9983347f40bf5ceb9cffb02e9bce2f0bb3239b6c
ruwpy <ruwpy.who@gmail.com>
2022-12-23 00:06:57 +0700

    change auth persist

--- src/App.jsx
+++ src/App.jsx
@@ -4,55 +4,39 @@ import Recipes from "./components/Recipes/Recipes";
 import './App.scss'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import { setUser } from "./store/slices/userSlice";
 import { useState } from "react";
 import Loading from "./components/Loading/Loading";
 import Profile from "./pages/Profile/Profile";
+import { supabase } from "./supabase";
 
 export default function App() {
 
   const dispatch = useDispatch()
-  const supabase = createClient(
-    import.meta.env.VITE_SUPA_URL,
-    import.meta.env.VITE_SUPA_KEY
-  )
-  const [isAuthLoading, setIsAuthLoading] = useState(true)
 
   useEffect(() => {
-    try {
-      async function getUserData() {
-        const {data} = await supabase.auth.getUser()
-        dispatch(setUser(data.user))
-      }
+    supabase.auth.getSession()
+      .then(({data: {session}}) => {
+        dispatch(setUser(session?.user))
+      })
 
-      getUserData()
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsAuthLoading(false)
-    }
-
+    supabase.auth.onAuthStateChange((_event, session) => {
+      dispatch(setUser(session.user))
+    })
   }, [])
 
   return (
     <BrowserRouter>
-      {!isAuthLoading ? (
-        <>
-          <header className="header">
-            <Search />
-            <Navbar />
-          </header>
-          <Routes>
-            <Route path='/' element={<Recipes />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/createrecipe' element={<Recipes />} />
-          </Routes>
-        </>
-      ) : (
-        <Loading />
-      ) }
+      <header className="header">
+        <Search />
+        <Navbar />
+      </header>
+      <Routes>
+        <Route path='/' element={<Recipes />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/createrecipe' element={<Recipes />} />
+      </Routes>
     </BrowserRouter>
   );
 }
