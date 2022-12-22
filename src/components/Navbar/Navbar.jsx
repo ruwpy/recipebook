23e809095e6872f01bfc19e1384dd627401e08ca
ruwpy <ruwpy.who@gmail.com>
2022-12-23 00:06:16 +0700

    change link to navlink, add loginmodal to jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,43 +2,44 @@ import './Navbar.scss'
 import addrecipe from '/addrecipe.svg'
 import book from '/book.svg'
 import userIcon from '/user.svg'
-import { Link } from 'react-router-dom'
-import { createClient } from "@supabase/supabase-js";
+import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import LoginModal from '../LoginModal/LoginModal'
+import { useState } from 'react'
 
 export default function Navbar() {
 
-  const navigate = useNavigate()
   const user = useSelector(state => state.user)
-  const supabase = createClient(
-    import.meta.env.VITE_SUPA_URL,
-    import.meta.env.VITE_SUPA_KEY
-  )
+  const [isLoginModalActive, setIsLoginModalActive] = useState(false)
 
-  async function Login() {
-    if(user.user?.id) {
-      navigate('/profile')
-    } else {
-      await supabase.auth.signInWithOAuth({
-        provider: 'google'
-      })
-    }
+  async function openLoginModal() {
+    setIsLoginModalActive(true)
   }
 
   return (
-    <nav className="nav">
-      <div className="nav__menu">
-        <Link to='/'>
-          <img className='nav__link-image' src={book} alt="recipe book icon" />
-        </Link>
-        <Link to='/profile'>
-          <img onClick={() => Login()} className='nav__link-image' src={userIcon} alt="add recipe icon" />
-        </Link>
-        <Link to='/createrecipe'>
-          <img className='nav__link-image' src={addrecipe} alt="add recipe icon" />
-        </Link>
-      </div>
-    </nav>
+    <>
+      <nav className="nav">
+        <div className="nav__menu">
+          <NavLink to='/'>
+            <img className='nav__link-image' src={book} alt="recipe book icon" />
+          </NavLink>
+          {
+            user.userData ? (
+              <NavLink to='/profile'>
+                <img className='nav__link-image' src={userIcon} alt="add recipe icon" />
+              </NavLink>
+            ) : (
+              <span className='nav__link' onClick={() => openLoginModal()}>
+                <img className='nav__link-image' src={userIcon} alt="add recipe icon" />
+              </span>
+            )
+          }
+          <NavLink to='/createrecipe'>
+            <img className='nav__link-image' src={addrecipe} alt="add recipe icon" />
+          </NavLink>
+        </div>
+      </nav>
+      <LoginModal isLoginModalActive={isLoginModalActive} setIsLoginModalActive={setIsLoginModalActive} />
+    </>
   )
 }
