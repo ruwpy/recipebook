84516fa05c12edc25117f7892f37a670c81d3b7d
ruwpy <ruwpy.who@gmail.com>
2023-01-07 17:31:10 +0700

    add dark mode switcher, update user login persist

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import { setUser } from "./store/slices/userSlice";
-import { useState } from "react";
-import Loading from "./components/Loading/Loading";
+import CreateRecipePage from "./pages/CreateRecipePage/CreateRecipePage";
 import Profile from "./pages/Profile/Profile";
 import { supabase } from "./supabase";
+import DarkModeSwitcher from "./components/DarkModeSwitcher/DarkModeSwitcher";
 
 export default function App() {
 
@@ -17,12 +17,34 @@ export default function App() {
 
   useEffect(() => {
     supabase.auth.getSession()
-      .then(({data: {session}}) => {
-        dispatch(setUser(session?.user))
+      .then(async ({data: {session}}) => {
+        const {data: profile, error} = await supabase
+          .from('profiles')
+          .select()
+          .eq('id', session.user.id)
+
+        if (profile) {
+          dispatch(setUser(profile[0]))
+        }
+
+        if (error) {
+          console.log(error);
+        }
       })
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      dispatch(setUser(session.user))
+    supabase.auth.onAuthStateChange(async (_event, session) => {
+      const {data: profile, error} = await supabase
+          .from('profiles')
+          .select()
+          .eq('id', session.user.id)
+
+        if (profile) {
+          dispatch(setUser(profile[0]))
+        }
+
+        if (error) {
+          console.log(error);
+        }
     })
   }, [])
 
@@ -31,11 +53,12 @@ export default function App() {
       <header className="header">
         <Search />
         <Navbar />
+        <DarkModeSwitcher />
       </header>
       <Routes>
         <Route path='/' element={<Recipes />} />
         <Route path='/profile' element={<Profile />} />
-        <Route path='/createrecipe' element={<Recipes />} />
+        <Route path='/createrecipe' element={<CreateRecipePage />} />
       </Routes>
     </BrowserRouter>
   );
