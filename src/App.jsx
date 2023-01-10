db77cc411753aa584d5d2a4e234db005fecae9ba
ruwpy <ruwpy.who@gmail.com>
2023-01-11 05:18:19 +0700

    wrapped app in main component, add ternary if there session

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Search from "./components/Search/Search";
 import Navbar from "./components/Navbar/Navbar";
 import Recipes from "./components/Recipes/Recipes";
-import './App.scss'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
@@ -10,6 +9,10 @@ import CreateRecipePage from "./pages/CreateRecipePage/CreateRecipePage";
 import Profile from "./pages/Profile/Profile";
 import { supabase } from "./supabase";
 import DarkModeSwitcher from "./components/DarkModeSwitcher/DarkModeSwitcher";
+import RecipePage from "./pages/RecipePage/RecipePage";
+import Main from "./components/Main/Main";
+import Landing from "./pages/Landing/Landing";
+
 
 export default function App() {
 
@@ -18,17 +21,19 @@ export default function App() {
   useEffect(() => {
     supabase.auth.getSession()
       .then(async ({data: {session}}) => {
-        const {data: profile, error} = await supabase
-          .from('profiles')
-          .select()
-          .eq('id', session.user.id)
-
-        if (profile) {
-          dispatch(setUser(profile[0]))
-        }
-
-        if (error) {
-          console.log(error);
+        if(session) {
+          const {data: profile, error} = await supabase
+            .from('profiles')
+            .select()
+            .eq('id', session.user.id)
+  
+          if (profile) {
+            dispatch(setUser(profile[0]))
+          }
+  
+          if (error) {
+            console.log(error);
+          }
         }
       })
 
@@ -55,11 +60,14 @@ export default function App() {
         <Navbar />
         <DarkModeSwitcher />
       </header>
-      <Routes>
-        <Route path='/' element={<Recipes />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/createrecipe' element={<CreateRecipePage />} />
-      </Routes>
+      <Main>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/createrecipe' element={<CreateRecipePage />} />
+          <Route path='/recipe/:id' element={<RecipePage />} />
+        </Routes>
+      </Main>
     </BrowserRouter>
   );
 }
