23527217b8cc437235c4b992cb82e7275d2a06fa
ruwpy <ruwpy.who@gmail.com>
2023-01-11 05:09:31 +0700

    moved modals to their directory

--- src/components/Modals/Login/LoginModal.jsx
+++ src/components/Modals/Login/LoginModal.jsx
@@ -1,6 +1,6 @@
 import Modal from "../Modal/Modal"
-import { supabase } from "../../supabase"
-import Button from "../Button/Button";
+import { supabase } from "../../../supabase"
+import Button from "../../Button/Button";
 import './LoginModal.scss'
 import google from '/google.svg'
 
@@ -15,12 +15,12 @@ export default function LoginModal({isLoginModalActive, setIsLoginModalActive}) 
 
   return (
     <Modal isModalActive={isLoginModalActive} setIsModalActive={setIsLoginModalActive}>
-      <div className="loginmodal">
-        <div className="loginmodal__top">
+      <div className="modal__content--initial">
+        <div className="loginmodal">
           <h2>Добро пожаловать на <span className="hl">RecipeBook</span></h2>
-          <p>для просмотра профиля вам необходимо войти в свою учетную запись</p>
+          <p>для просмотра этой страницы вам необходимо войти в свою учетную запись</p>
         </div>
-        <Button type='secondary' hasIcon={true} onClick={() => Login()}>
+        <Button type='secondary w100' hasIcon={true} onClick={() => Login()}>
           <span>Войти с помощью</span>
           <img src={google} alt="google icon" />
         </Button>
