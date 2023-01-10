98cb72f3ce71f7379d7ecf5784ad8927921bba20
ruwpy <ruwpy.who@gmail.com>
2023-01-11 05:19:02 +0700

    add new functions to user slice

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -11,10 +11,21 @@ export const userSlice = createSlice({
       const user = action.payload
       state.userData = user
       state.isLoading = false
+    },
+    setNewUsername(state, action) {
+      const username = action.payload
+      state.userData.username = username
+    },
+    clearUserdata(state) {
+      state.userData = {}
+    },
+    setUserLoading(state, action) {
+      const isLoading = action.payload
+      state.isLoading = isLoading
     }
   }
 })
 
-export const { setUser } = userSlice.actions
+export const { setUser, setNewUsername, clearUserdata, setUserLoading } = userSlice.actions
 
 export default userSlice.reducer
