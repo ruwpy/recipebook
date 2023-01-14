7d0367e8967747689b6e2f39bac076c26671e890
ruwpy <ruwpy.who@gmail.com>
2023-01-14 13:35:26 +0700

    change components layout

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -4,27 +4,31 @@ import { supabase } from "../../supabase"
 import './Recipes.scss'
 import { useSelector } from "react-redux"
 
-export default function Recipes() {
+export default function Recipes({type}) {
 
+  const user = useSelector(state => state.user.userData)
   const [recipes, setRecipes] = useState([])
   const searchQuery = useSelector(state => state.search.query)
 
   useEffect(() => {
     async function getRecipes() {
-      if(searchQuery) {
-        const {data, error} = await supabase
-          .from('global-recipes')
-          .select()
-          .textSearch('title', `'${searchQuery}'`)
-          console.log(data);
-          
-        setRecipes(data)
-      } else {
-        const {data, error} = await supabase
-          .from('global-recipes')
-          .select()
-
-        setRecipes(data)
+      try {
+        if(searchQuery) {
+          const {data} = await supabase
+            .from('recipes')
+            .select()
+            .ilike('title',  `%${searchQuery}%`)
+            
+          setRecipes(data)
+        } else {
+          const {data} = await supabase
+            .from('recipes')
+            .select()
+  
+          setRecipes(data)
+        }
+      } catch (error) {
+        console.log(error);
       }
     }
 
@@ -32,24 +36,21 @@ export default function Recipes() {
   }, [searchQuery])
 
   return (
-    <main className="main">
-      <div className="container">
-        <section className="recipes grid grid--recipes">
-          {recipes.map((recipe) => {
-            return (
-              <SingleRecipe
-                key={recipe.id}
-                title={recipe.title}
-                image={recipe.image}
-                rating={recipe.rating}
-                author={recipe.author}
-                isSpicy={recipe.isSpicy}
-                timeToCook={recipe.timeToCook}
-              />
-            )
-          })}
-        </section>
-      </div>
-    </main>
+    <section className="recipes grid grid--recipes">
+      {recipes && recipes.map((recipe) => {
+        return (
+          <SingleRecipe
+            key={recipe.id}
+            title={recipe.title}
+            image={recipe.image}
+            rating={recipe.rating}
+            author={recipe.author}
+            isSpicy={recipe.is_spicy}
+            timeToCook={recipe.time_to_cook}
+            id={recipe.id}
+          />
+        )
+      })}
+    </section>
   )
 }
--- src/components/SingleRecipe/SingleRecipe.jsx
+++ src/components/SingleRecipe/SingleRecipe.jsx
@@ -1,15 +1,17 @@
 import './SingleRecipe.scss'
-import like from '/like.svg'
-import dislike from '/dislike.svg'
-import fire from '/fire.svg'
 import DescriptionOnHover from '../DescriptionOnHover/DescriptionOnHover'
+import { useNavigate } from 'react-router-dom'
+import fire from '/fire.svg'
+import timer from '/timer.svg'
 
-export default function SingleRecipe({image, title, rating, author, isSpicy, timeToCook}) {
+export default function SingleRecipe({image, title, author, isSpicy, timeToCook, id}) {
+
+  const navigate = useNavigate()
 
   return (
-    <div className="recipe">
+    <div onClick={() => navigate('/recipe/' + id)} className="recipe">
       <div className="recipe__container">
-        <img className="recipe__image" src={image} alt="recipe image" />
+        <img className="recipe__image" src={'https://fddqymmrolabclbttaxr.supabase.co/storage/v1/object/public/recipe-images/' + image} alt="recipe image" />
         {isSpicy &&
         <span className='recipe__hot'>
           <img src={fire} alt="spicy icon" />
@@ -18,20 +20,9 @@ export default function SingleRecipe({image, title, rating, author, isSpicy, tim
         }
       </div>
       <span className="recipe__title">{title}</span>
-      <span className='recipe__author'>от: {author}</span>
-      <div className='recipe__bottom'>
-        <span className='recipe__time'>Время приготовления - <span className="hl">{timeToCook}</span> минут</span>
-        <span className="recipe__rating">
-          <span className="recipe__rating--likes">
-            <img src={like} alt="like icon" />
-            {rating.likes}
-          </span>
-          <span className="recipe__rating--dislikes">
-            <img src={dislike} alt="dislike icon" />
-            {rating.dislikes}
-          </span>
-        </span>
-      </div>
+      <span className='recipe__time'>
+        <img src={timer} alt="timer icon" /> - {timeToCook === '0' ? '< 10' : timeToCook} мин.
+      </span>
     </div>
   )
 }
