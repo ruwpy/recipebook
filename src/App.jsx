712b1f6e95f4842d4fc09b04ec8c4c542dfacc47
ruwpy <ruwpy.who@gmail.com>
2023-01-14 13:33:48 +0700

    remove filter, search components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import Search from "./components/Search/Search";
 import Navbar from "./components/Navbar/Navbar";
-import Recipes from "./components/Recipes/Recipes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
@@ -8,7 +6,6 @@ import { setUser } from "./store/slices/userSlice";
 import CreateRecipePage from "./pages/CreateRecipePage/CreateRecipePage";
 import Profile from "./pages/Profile/Profile";
 import { supabase } from "./supabase";
-import DarkModeSwitcher from "./components/DarkModeSwitcher/DarkModeSwitcher";
 import RecipePage from "./pages/RecipePage/RecipePage";
 import Main from "./components/Main/Main";
 import Landing from "./pages/Landing/Landing";
@@ -56,7 +53,6 @@ export default function App() {
   return (
     <BrowserRouter>
       <header className="header">
-        <Search />
         <Navbar />
       </header>
       <Main>
