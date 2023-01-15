0f934a7754869d82dafce4d12686cda52178e6bf
ruwpy <ruwpy.who@gmail.com>
2023-01-15 13:34:43 +0700

    create landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import './Landing.scss'
+import bludo from '/bludo.webp'
+import Button from '../../components/Button/Button'
 
 export default function Landing() {
   return (
-    <div className='landing container'>Landing</div>
+    <div className='landing container'>
+      <div className="landing__bg" />
+      <div className="landing__content">
+        <h1 className='landing__heading'>RecipeBook</h1>
+        <span className='landing__desc'>ваша личная книга рецептов</span>
+        <span className='landing__button'>
+          <Button type='primary'>
+            Перейти к рецептам
+          </Button>
+        </span>
+      </div>
+      <div className="landing__img">
+        <img src={bludo} alt="dish image" />
+      </div>
+    </div>
   )
 }
